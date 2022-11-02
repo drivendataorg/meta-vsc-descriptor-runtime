4e7a4d6e6f44f637722b81c259e691a564fe1b9a
Chris Kucharczyk <chris@drivendata.org>
2022-11-02 15:11:32 -0500

    remove unused import

--- benchmark_src/main.py
+++ benchmark_src/main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import shlex
 import subprocess
-from time import sleep
 
 ROOT_DIRECTORY = Path("/code_execution/")
 DATA_DIRECTORY = Path("/data")
@@ -11,14 +10,17 @@ QUERY_SUBSET_FILE = DATA_DIRECTORY / "query_subset.csv"
 
 
 def main():
-    command = shlex.split(f"""
+    command = shlex.split(
+        f"""
     conda run --no-capture-output -n condaenv python -m vsc.baseline.inference
         --torchscript_path "/code_execution/vsc2022/vsc/baseline/adapted_sscd_disc_mixup.torchscript.pt"
         --accelerator=cuda --processes="1"
         --dataset_path "{QUERY_SUBSET_VIDEOS_FOLDER}"
         --output_file "{OUTPUT_FILE}"
-    """)
+    """
+    )
     subprocess.run(command, cwd=(ROOT_DIRECTORY / "vsc2022"))
+
 
 if __name__ == "__main__":
     main()
