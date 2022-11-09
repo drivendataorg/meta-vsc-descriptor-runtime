35edf7b38176acdeb3aa639c5c216426e3a77675
Chris Kucharczyk <chris@drivendata.org>
2022-11-09 17:34:11 -0600

    use approprate constants vs variables

--- scripts/generate_data_subset.py
+++ scripts/generate_data_subset.py
@@ -21,48 +21,49 @@ parser.add_argument(
     choices=["train", "test", "phase2"],
 )
 
+RUNTIME_DATA_DIR = Path(__file__).parent.parent / "data"
+QUERY_SUBSET_DIR = RUNTIME_DATA_DIR / "queries"
+
 
 def main(args: Namespace):
     # Ensure that data has been downloaded to the correct place
-    COMPETITION_DATA_DIR = Path("competition_data")
-    DATASET_FOLDER = COMPETITION_DATA_DIR / args.dataset
-    QUERY_VIDEO_DIR = DATASET_FOLDER / "queries"
+    competition_data_dir = Path("competition_data")
+    dataset_folder = competition_data_dir / args.dataset
+    query_video_dir = dataset_folder / "queries"
 
-    QUERY_METADATA = DATASET_FOLDER / "query_metadata.csv"
-    REFERENCE_METADATA = DATASET_FOLDER / "reference_metadata.csv"
+    query_metadata = dataset_folder / "query_metadata.csv"
+    reference_metadata = dataset_folder / "reference_metadata.csv"
 
     for file in [
-        COMPETITION_DATA_DIR,
-        DATASET_FOLDER,
-        QUERY_METADATA,
-        REFERENCE_METADATA,
+        competition_data_dir,
+        dataset_folder,
+        query_metadata,
+        reference_metadata,
     ]:
         if not file.exists():
             raise FileExistsError(
-                f"Error: {file} not found. Have you downloaded or symlinked the competition dataset into {COMPETITION_DATA_DIR}?"
+                f"Error: {file} not found. Have you downloaded or symlinked the competition dataset into {competition_data_dir}?"
             )
 
     p_subset = args.subset_fraction
 
     # Choose a random subset of query IDs to include
     rng = np.random.RandomState(42)
-    query_metadata = pd.read_csv(QUERY_METADATA)
+    query_metadata = pd.read_csv(query_metadata)
     subset_query_ids = query_metadata.video_id.sample(
         int(np.ceil(query_metadata.shape[0] * p_subset)), random_state=rng
     )
 
     # Copy metadata files and chosen videoss
-    RUNTIME_DATA_DIR = Path("data")
-    QUERY_SUBSET_DIR = RUNTIME_DATA_DIR / "queries"
 
     subset_query_ids.to_csv(RUNTIME_DATA_DIR / "query_subset.csv", index=False)
     for query_id in subset_query_ids:
         copyfile(
-            QUERY_VIDEO_DIR / f"{query_id}.mp4",
+            query_video_dir / f"{query_id}.mp4",
             QUERY_SUBSET_DIR / f"{query_id}.mp4",
         )
-    copyfile(QUERY_METADATA, RUNTIME_DATA_DIR / QUERY_METADATA.name)
-    copyfile(REFERENCE_METADATA, RUNTIME_DATA_DIR / REFERENCE_METADATA.name)
+    copyfile(query_metadata, RUNTIME_DATA_DIR / query_metadata.name)
+    copyfile(reference_metadata, RUNTIME_DATA_DIR / reference_metadata.name)
 
 
 if __name__ == "__main__":
